a3284468beebe53cdd78abc2489223d6b4486398
Janeen Scott <janeen.e.scott@gmail.com>
2019-01-31 18:28:13 -0500

    finished select statemtents

--- app.sql
+++ app.sql
@@ -1,9 +1,34 @@
+-- SELECT
+-- "Movies".id, "Person"."Age", "Review"."Movie"
+-- FROM
+-- "Review"
+-- INNER JOIN "Movies"
+-- ON	"Review"."Movie" = "Movies".id
+-- INNER JOIN "Person"
+-- ON "Review"."Person" = "Person".id
+-- WHERE "Movies"."Title" = 'Toy Story'
+
+-- SELECT 
+-- "Occupation"
+-- FROM
+-- "Person"
+-- WHERE
+-- "Age"< 40
+
+-- SELECT * 
+-- FROM
+-- "Movies"
+-- WHERE
+-- "IMDB Url" IS NULL
+
 SELECT
-"Person"."Age"
+"Movies"."Title", "Review"."Rating"
 FROM
 "Review"
-	JOIN "Movies"
-		ON	"Movies".id = "Review"."Movie"
-	JOIN "Person"
-		ON "Person"."Age" = "Review"."Person"
-WHERE NCHAR("Movie"."Title") = "Toy Story"
+INNER JOIN
+"Movies"
+ON
+"Movies".id="Review"."Movie"
+WHERE
+"Rating" < 2
+
